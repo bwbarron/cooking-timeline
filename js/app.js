d79d0c69982d38f21f004940bcb3bc6c7fa5f2cd
bwbarron <brandon.barron@live.com>
2015-12-06 11:53:50 -0800

    added angular skeleton to app.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+angular.module("Timeline", ["ui.router"])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        // ui routing config goes here
+    })
+    .controller("FormController", function ($scope) {
+        "use strict";
+
+    })
+    .controller("TimelineController", function ($scope) {
+        "use strict";
+
+    });
