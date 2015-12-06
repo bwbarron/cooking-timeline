631f10feacdec28cccf2a9c4cb9f0c22a8012b68
bradholland84 <bholland.mail@gmail.com>
2015-12-06 11:00:20 -0800

    moved js files

---
+++ js/timline.js
@@ -0,0 +1,49 @@
+/**
+ * Created by iguest on 12/6/15.
+ */
+$(document).ready(function() {
+    var box = $('#box');
+    var box2 = $('#box2');
+    var timeline = new TimelineLite({
+        onStart: start,
+        onUpdate: update,
+        onComplete: complete
+    });
+    var box2Time = 3;
+
+    // ( variable,
+    // animation length,
+    // {
+    // css properties
+    // },
+    // position in timeline
+    // )
+
+    timeline.to(
+        box,
+        1,
+        {
+            left: 0
+        }
+    ).to(
+        box2,
+        1,
+        {
+            left: 0
+        }
+
+    );
+
+    //callback functions for elemtents if we want
+    function start() {
+        console.log('timeline started');
+    }
+
+    function update() {
+        console.log('update');
+    }
+
+    function complete() {
+        console.log('complete');
+    }
+});
